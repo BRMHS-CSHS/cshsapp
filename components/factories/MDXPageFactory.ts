ed8f240b6e2a398fd247d55a1471626a6330d122
Damien Vesper <ldamienvesper@gmail.com>
2024-08-22 03:02:45 -0500

    ci test 3

--- components/factories/MDXPageFactory.ts
+++ components/factories/MDXPageFactory.ts
@@ -6,10 +6,12 @@ interface IOpts {
     path: string
 }
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface IParams {}
 
 export default function MDXPageFactory (args: IOpts) {
     return async function MDXPage ({ params }: { params: IParams }) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const files = await readdir(resolve(fileURLToPath(import.meta.url), `../../app/(cshs)/${args.path}/articles`), { withFileTypes: true });
     };
 }
