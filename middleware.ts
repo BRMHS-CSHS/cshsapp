d7593440308e484b970b961733a2345a96c5efde
charlesbae <90109397+charlesbae@users.noreply.github.com>
2024-09-01 09:29:24 -0500

    update: furthered middleware for non-users as well

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,16 @@
 import { auth } from "@/auth";
 import { NextRequest, NextResponse } from "next/server";
 
-const protectedRoutes = ["/admin"];
+const protectedRoutes_admin = ["/admin"];
+const protectedRoutes_user = ["/dashboard", "/contact_page", "/settings", "/service-history"];
 
 export async function middleware (req: NextRequest): Promise<any> {
     const session = (await auth());
-    if ((session?.user as any)?.role !== "Admin" && protectedRoutes.includes(req.nextUrl.pathname)) {
+    if ((session?.user as any)?.role !== "Admin" && protectedRoutes_admin.includes(req.nextUrl.pathname)) {
+        const url = new URL("/", req.nextUrl.origin);
+        return NextResponse.redirect(url.toString());
+    }
+    if (!session?.user && protectedRoutes_user.includes(req.nextUrl.pathname)) {
         const url = new URL("/", req.nextUrl.origin);
         return NextResponse.redirect(url.toString());
     }
