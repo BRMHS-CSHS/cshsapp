d29f6ae81ec6e82972e0db265361a60fd2a50d75
charlesbae <90109397+charlesbae@users.noreply.github.com>
2024-09-01 12:22:32 -0500

    some buttons, /settings update

--- app/(cshs)/(user)/login/page.tsx
+++ app/(cshs)/(user)/login/page.tsx
@@ -24,15 +24,16 @@ export default function Home (): React.ReactElement {
 
     return (
         <div className="flex justify-center items-center">
-            <section className="size-96 outline outline-zinc-900 bg-gray-800 rounded-large p-10">
+            <section className="size-96 outline outline-zinc-900 bg-gray-800 rounded-large p-10 space-y-4 flex flex-col justify-between">
                 <FormControl className="space-y-2 flex flex-col">
                     <FormLabel className="font-bold flex justify-center"><h1>Login</h1></FormLabel>
 
                     <Input type="email" label="Email" className="max-w-xs" onChange={e => setEmail(e.target.value)} />
                     <Input type="password" label="Password" className="max-w-xs" onChange={e => setPassword(e.target.value)} />
 
-                    <Button type="submit" className="max-w-xs" onPress={handleClick}>Submit</Button>
+                    <Button type="submit" className="max-w-xs" color="success" variant="ghost" onPress={handleClick}>Submit</Button>
                 </FormControl>
+                <Button type="submit" className="max-w-xs" variant="ghost" color="danger">Forgot Password?</Button>
             </section>
         </div>
     );
--- app/(cshs)/(user)/settings/page.tsx
+++ app/(cshs)/(user)/settings/page.tsx
@@ -1,7 +1,50 @@
-import { Settings } from "@/components/settings";
+"use client";
 
-export default function Page (): React.ReactElement {
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    Heading,
+    Stack,
+    StackDivider
+} from "@chakra-ui/react";
+
+import { Button } from "@nextui-org/react";
+import { Link } from "@nextui-org/react";
+import { useSessionData } from "@/lib/auth/useSessionData";
+
+export default function Dashboard (): React.ReactElement {
+    const session = useSessionData(); // guarenteed to be non-null
+
+    const User = {
+        name: session.data?.user?.name,
+        email: session.data?.user?.email,
+        role: (session.data?.user as any)?.role,
+        hours: (session.data?.user as any)?.hours,
+        isLoggedIn: session.data?.user ? true : false
+    };
+
     return (
-        <Settings />
+        <div className="flex flex-col justify-center items-center">
+            <div className="bg-gray-800 outline outline-4 outline-zinc-800 rounded-large p-20 space-y-10 flex">
+                <Card className="space-y-10 flex flex-col items-center">
+                    <CardHeader>
+                        <Heading size="xl" className="font-extrabold text-3xl">Welcome, {User.name}</Heading>
+                    </CardHeader>
+
+                    <CardBody className="">
+                        <Stack divider={<StackDivider />} spacing="10">
+                            <div className="font-bold text-xl space-y-4">
+                                <div className="flex justify-center items-center space-x-4">
+                                    <Link href="/service-history">
+                                        <Button variant="ghost" color="warning">Change Password</Button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </Stack>
+                    </CardBody>
+                </Card>
+            </div>
+        </div>
     );
 }
